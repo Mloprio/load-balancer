f97fdc45683574515fd09832bd914c56b800cd5f
Mloprio <miglopezrios@gmail.com>
2024-06-02 16:35:47 +0200

    Profile added to MainController

--- src/main/java/com/mloprio/controller/MainController.java
+++ src/main/java/com/mloprio/controller/MainController.java
@@ -2,12 +2,14 @@ package com.mloprio.controller;
 
 import com.mloprio.LoadBalancer;
 import com.mloprio.Server;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Profile("lb")
 public class MainController {
 
     private final LoadBalancer loadBalancer;
